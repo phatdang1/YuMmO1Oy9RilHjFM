45bc0b8135f35e63e07e8735456c434ead0f8bef
phatdang1 <dangminhphat1@gmail.com>
2022-06-15 15:56:26 -0400

    Second Commit

--- HappyCustomer.py
+++ HappyCustomer.py
@@ -3,21 +3,28 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
 
 #loading data
 review_data = pd.read_csv(r'ACME-HappinessSurvey2020.csv')
 
 list_of_review = review_data.columns.values.tolist()
 
-X = review_data.drop('Y', axis=1)
+review_data = review_data.drop(['X4', 'X6'], axis=1 )
+X = review_data.drop(['Y'], axis=1)
 y = review_data['Y']
 
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-classifier = RandomForestClassifier(n_estimators=30, random_state=0)
+classifier = RandomForestClassifier(n_estimators=10, random_state=0)
 
 classifier.fit(x_train, y_train)
 
 prediction = classifier.predict(x_test)
 
 print(prediction)
+print(classification_report(y_test, prediction))
+print(accuracy_score(y_test, prediction))
+
+important_feats = pd.Series(classifier.feature_importances_, index = X.columns)
+print(important_feats.nlargest(10))
